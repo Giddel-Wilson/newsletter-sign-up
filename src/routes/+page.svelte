3dea3690097ac3e6db6374f741d8f4eced0ef074
Giddel Wilson <134491076+Giddel-Wilson@users.noreply.github.com>
2024-10-09 15:34:16 +0100

    Add files via upload

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -159,9 +159,9 @@
 
   @media only screen and (max-width: 330px) {
     .card-content {
-      height: vh;
+      height: 120vh;
       min-height: calc(100vh - (env(safe-area-inset-bottom) + env(safe-area-inset-top)));
-      padding: env(safe-area-inset-top) 1rem env(safe-area-inset-bottom) 1rem;
+      padding: 2rem 1rem;
     }
   }
 
